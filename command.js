fb22713f0bab1df4de27c78fa5b84454da334aa5
Ivanguijarro <iguijarrolillo@gmail.com>
2021-06-09 22:12:54 +0200

    8

--- command.js
+++ command.js
@@ -1,22 +1,20 @@
-const { prefix } = require('./config.json')
-
+const { prefix } = require("./config.json");
 
 module.exports = (client, aliases, callback) => {
-    if (typeof aliases === 'string'){
-        aliases = [aliases]
-    }
-    
-    client.on('message', message => {
-        const { content } = message;
+  if (typeof aliases === "string") {
+    aliases = [aliases];
+  }
 
-        aliases.forEach(alias => {
-            const  command = `${prefix}${alias}`
+  client.on("message", (message) => {
+    const { content } = message;
 
-            if(content.startsWith(`${command} `) || content === command){
-                console.log(`Running the command ${command}`)
-                callback(message)
-            }
-        })
-    })
+    aliases.forEach((alias) => {
+      const command = `${prefix}${alias}`;
 
-}
+      if (content.startsWith(`${command} `) || content === command) {
+        console.log(`Running the command ${command}`);
+        callback(message);
+      }
+    });
+  });
+};
--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const config = require('./config.json')
-const command = require('./command')
+const config = require("./config.json");
+const command = require("./command");
 
-client.on('ready', () => {
-    console.log('The client is ready!')
+client.on("ready", () => {
+  console.log("The client is ready!");
 
-    command(client, 'ping', (message) => {
-        message.channel.send('Pong!')
-    })
-    command(client, 'f', (message) => {
-        message.channel.send('F')
-    })
-})
+  command(client, "ping", (message) => {
+    message.channel.send("Pong!");
+  });
+  command(client, "f", (message) => {
+    message.channel.send("F");
+  });
+  command(client, "usuarios", (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} tiene un total de ${guild.memberCount} miembros`
+      );
+    });
+  });
+  command(client, ["clear", "borrar"], async (message) => {
+    const numMsgs = message.content.split(" ")[1];
+    const promises = [];
+    if (!numMsgs || isNaN(numMsgs) || numMsgs < 0)
+      return await message.channel.send(
+        "Comando incorrecto, indica el numero de mensajes!"
+      );
 
-client.login(config.token)
-client.login(process.env.poro_token)
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      await message.channel.messages
+        .fetch({ limit: parseInt(numMsgs) > 50 ? 50 : parseInt(num) + 1 })
+        .then(async (results) => {
+          results.forEach((m) => {
+            promises.push(m.delete());
+          });
+          await Promise.all(promises);
+        });
+    }
+  });
+  command(client, "status", (message) => {
+    const content = message.content.replace("/status ", "");
+
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    });
+  });
+});
+
+client.login(config.token);
+//client.login(process.env.poro_token)
