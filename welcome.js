a985eb3adc018cf2bffae7927963b794977fed90
Ivanguijarro <iguijarrolillo@gmail.com>
2021-06-28 11:37:42 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -8,7 +8,7 @@ module.exports = client => {
 
     const cache = {}
     command(client, 'setwelcome', async (message) => {
-        const { member, channel, content, guild, text} = message
+        const { member, channel, content, guild} = message
 
         if(!member.hasPermission('ADMINISTRATOR')){
             channel.send('No tienes permisos para usar ese comando')
