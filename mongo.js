46302c5190ae944b90d98907374b807515c0d0ca
Ivanguijarro <iguijarrolillo@gmail.com>
2021-06-28 11:27:54 +0200

    actualizado el link de la database

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //const { mongoPath } = require('./config.json')
-const mongoPath = 'mongodb+srv://ivanguijarro:Ivanguijarro99@poro-bot.umx20.mongodb.net/poro-bot?retryWrites=true&w=majority'
+const mongoPath = 'mongodb+srv://ivanguijarro:Ivanguijarro99@poro-bot.umx20.mongodb.net/porobot?retryWrites=true&w=majority'
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
