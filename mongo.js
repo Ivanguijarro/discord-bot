8184978b3cf4b7fb0ca6e2f59499f854eeabd466
Ivanguijarro <iguijarrolillo@gmail.com>
2021-06-28 11:19:50 +0200

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const { mongoPath } = require('./config.json')
+//const { mongoPath } = require('./config.json')
+const mongoPath = 'mongodb+srv://ivanguijarro:Ivanguijarro99@poro-bot.umx20.mongodb.net/poro-bot?retryWrites=true&w=majority'
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
