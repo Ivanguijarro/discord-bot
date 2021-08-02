eb5a7e7fe137f688a64600620dd6636e999c64df
Ivanguijarro <iguijarrolillo@gmail.com>
2021-08-02 13:43:24 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -6,10 +6,16 @@ module.exports = (client) => {
     //console.log(member)
     console.log("Nuevo miembro")
 
-    const message = `¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`
+    //const message = `¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`
 
-    const channel = member.guild.channels.cache.get(channelId)
-    channel.send(message)
+    let welcomembed = new Discord.MessageEmbed()
+        .setAuthor(`${member.user.tag} just joined!`, member.user.avatarURL())
+        .setDescription(`¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`)
+        .setColor("FF0000");
+    member.guild.channels.cache.get("channelid").send(welcomembed)
+
+    //const channel = member.guild.channels.cache.get(channelId)
+    //channel.send(message)
   })
   client.on('guildMemberRemove', (member) => {
     //console.log(member)
