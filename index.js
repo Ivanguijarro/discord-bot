9719075c8e45561ab0e7ef50201e249ab1b0104d
Ivanguijarro <iguijarrolillo@gmail.com>
2021-08-02 13:11:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -242,6 +242,18 @@ client.on("ready", async () => {
     }
   })
 });
+const channelId = '470503733613035526'
+const targetChannel = '829296012945784864'
+
+client.on('guildMemberAdd', (member) => {
+  console.log(member)
+  console.log("Nuevo miembro")
+
+  const message = `¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`
+
+  const channel = member.guild.channels.cache.get(channelId)
+  channel.send(message)
+})
 
 client.login(config.token);
 client.login(process.env.poro_token);
