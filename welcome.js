508e1742b2832f65f2a8bb3ef193f9d0bb52f1f2
Ivanguijarro <iguijarrolillo@gmail.com>
2021-08-02 13:55:16 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,30 +1,30 @@
+const Discord = require("discord.js");
+
 module.exports = (client) => {
   const channelId = '470503733613035526'
+
   const targetChannel = '829296012945784864'
 
   client.on('guildMemberAdd', (member) => {
     //console.log(member)
     console.log("Nuevo miembro")
 
-    //const message = `¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`
-
     const welcomembed = new Discord.MessageEmbed()
         .setAuthor(`¡${member.user.tag} se ha unido!`, member.user.avatarURL())
         .setDescription(`¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`)
-        .setColor("FF0000");
+        .setColor("#3374FF");
     const channel = member.guild.channels.cache.get(channelId)
     channel.send(welcomembed)
-
-    //const channel = member.guild.channels.cache.get(channelId)
-    //channel.send(message)
   })
   client.on('guildMemberRemove', (member) => {
     //console.log(member)
     console.log("Fuera miembro")
 
-    const message = `<@${member.id}> se ha ido, ojalá vuelva`
-
+    const welcomembed = new Discord.MessageEmbed()
+        .setAuthor(`¡${member.user.tag} se ha ido!`, member.user.avatarURL())
+        .setDescription("¡Esperamos que vuelva pronto!")
+        .setColor("#3374FF");
     const channel = member.guild.channels.cache.get(channelId)
-    channel.send(message)
+    channel.send(welcomembed)
   })
 } 
