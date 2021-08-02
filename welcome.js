770f02665f50c1914025295096a07d90090bc0e8
Ivanguijarro <iguijarrolillo@gmail.com>
2021-08-02 13:33:11 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -3,7 +3,7 @@ module.exports = (client) => {
   const targetChannel = '829296012945784864'
 
   client.on('guildMemberAdd', (member) => {
-    console.log(member)
+    //console.log(member)
     console.log("Nuevo miembro")
 
     const message = `¡Bienvenido al servidor <@${member.id}>! Porfavor revisa las ${member.guild.channels.cache.get(targetChannel).toString()}`
@@ -12,7 +12,7 @@ module.exports = (client) => {
     channel.send(message)
   })
   client.on('guildMemberRemove', (member) => {
-    console.log(member)
+    //console.log(member)
     console.log("Fuera miembro")
 
     const message = `<@${member.id}> se ha ido, ojalá vuelva`
